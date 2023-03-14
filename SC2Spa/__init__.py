b8b5ecbb41c4ba7bee84e38ef9c2532e8d16666a
Linbu Liao <linbu.liao@gmail.com>
2023-03-14 17:35:07 +0100

    Update Tutorial1

---
+++ SC2Spa/__init__.py
@@ -0,0 +1,11 @@
+from .__metadata__ import (
+        __author__,
+        __copyright__,
+        __email__,
+        __license__,
+        __summary__,
+        __title__,
+        __uri__,
+        __version__,
+        __all__
+    )
---
+++ SC2Spa/__metadata__.py
@@ -0,0 +1,24 @@
+
+__title__ = "sc2spa"
+__summary__ = "A method for mapping single cells to space"
+__uri__ = "https://github.com/linbuliao/SC2Spa"
+
+__version__ = "1.1.5"
+
+
+__author__ = "Linbu Liao, Won Lab"
+__email__ = "linbu.liao@gmail.com"
+
+__license__ = "BSD 3-Clause License"
+__copyright__ = "2022 %s" % __author__
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
