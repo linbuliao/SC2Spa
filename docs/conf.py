32d26c6194296acfe25c010636ac36badce633f6
Linbu Liao <linbu.liao@gmail.com>
2023-07-31 19:42:02 +0200

    Update API design and documentation

--- docs/conf.py
+++ docs/conf.py
@@ -14,10 +14,10 @@ import sphinx_rtd_theme
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+#sys.path.insert(0, os.path.abspath('.'))
 from pathlib import Path
-HERE = Path(__file__).parent
-sys.path.insert(0, str(HERE / "extensions"))
+HERE = Path(__file__).parent.parent
+sys.path.insert(0, str(HERE))
 
 import SC2Spa
 
---
+++ docs/test.py
@@ -0,0 +1,26 @@
+import sphinx_rtd_theme
+
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+from pathlib import Path
+HERE = Path(__file__).parent.parent
+sys.path.insert(0, HERE)
+
+print(os.path.abspath('.'))
+print(HERE)
+import SC2Spa
+
+
